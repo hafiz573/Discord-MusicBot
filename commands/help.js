66c8b08dd9660ccf9cebbfe976a15805124ef68c
HZR_H3RY <79120962+hafiz573@users.noreply.github.com>
2021-08-01 22:21:38 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -36,10 +36,7 @@ module.exports = {
               }help [Command] | Have a nice day!`
             ).setDescription(`${Commands.join("\n")}
   
-  Discord Music Bot Version: v${require("../package.json").version}
-  [✨ Support Server](${
-    client.config.SupportServer
-  }) | [GitHub](https://github.com/SudhanPlayz/Discord-MusicBot) | By [SudhanPlayz](https://github.com/SudhanPlayz)`);
+  Discord Music Bot Version: v${require("../package.json").version}`);
     if (!args[0]) message.channel.send(Embed);
     else {
       let cmd =
